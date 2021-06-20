7da2d344e807ecef0c70502b9269f11c7135486e
Y.Maeshima <ymzlgcranch@gmail.com>
2021-06-21 01:53:26 +0900

    Deployerを使えるようにした

--- deploy.php
+++ deploy.php
@@ -4,37 +4,38 @@ namespace Deployer;
 require 'recipe/laravel.php';
 
 // Project name
-set('application', 'my_project');
+set('application', 'among-tusk');
 
 // Project repository
 set('repository', 'git@github.com:cranch-crown/among-tusk.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', true);
 
-// Shared files/dirs between deploys 
+// Shared files/dirs between deploys
 add('shared_files', []);
-add('shared_dirs', []);
+add('shared_dirs', ['vendor']);
 
-// Writable dirs by web server 
-add('writable_dirs', []);
+// Writable dirs by web server
+add('writable_dirs', ['storage']);
 
 
 // Hosts
+host('160.251.55.88')
+    ->user('login_user')
+    ->identityFile('~/.ssh/id_ed25519')
+    ->set('deploy_path', '/var/www/{{application}}');
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
-    
 // Tasks
 
-task('build', function () {
-    run('cd {{release_path}} && build');
-});
+//task('build', function () {
+//    run('cd {{release_path}} && build');
+//});
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-before('deploy:symlink', 'artisan:migrate');
+//before('deploy:symlink', 'artisan:migrate');
 
