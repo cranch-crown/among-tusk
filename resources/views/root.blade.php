9675edf39ee9833e8db867f7d6510e45367cf6b6
Y.Maeshima <ymzlgcranch@gmail.com>
2021-06-26 12:14:17 +0900

    バグ報告リンク作成

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -5,4 +5,5 @@
     <p>「Among us」の縛りプレイのお題を選べるサイトです。</p>
     <p>現在 {{ $taskCount }} 種類の縛りが登録されています。</p>
     <a href="{{ route('task') }}">自分の縛りを抽選する</a>
+    <p>バグが有りましたら<a href="https://forms.gle/YcecXwXhA76zBqfW7">こちら</a>からご連絡いただけると嬉しいです。</p>
 @endsection
